ee38496d822480aaaf257e612f7c33350d3637dd
andyreesecups <andyreesecups@yahoo.com>
2017-08-17 19:01:23 -0500

    added ball click event, randomized ball speed and direction

--- pong.js
+++ pong.js
@@ -48,11 +48,6 @@ class Pong {
         this._context = canvas.getContext('2d');
 
         this.ball = new Ball;
-        this.ball.pos.x = 100;
-        this.ball.pos.y = 50;
-
-        this.ball.vel.x = 100;
-        this.ball.vel.y = 100;
 
         this.players = [
             new Player,
@@ -75,6 +70,8 @@ class Pong {
             requestAnimationFrame(callback);
         };
         callback();
+
+        this.reset();
     }
 
     collide(player, ball) {
@@ -96,12 +93,28 @@ class Pong {
         this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
     }
 
+    reset() {
+        this.ball.pos.x = this._canvas.width / 2;
+        this.ball.pos.y = this._canvas.height / 2;
+        this.ball.vel.x = 0;
+        this.ball.vel.y = 0;
+    }
+
+    start() {
+        if (this.ball.vel.x === 0 && this.ball.vel.y === 0) {
+            this.ball.vel.x = 300 * (Math.random() > .5 ? 1 : -1);
+            this.ball.vel.y = 300 * (Math.random() * 2 - 1);
+        }
+    }
+
     update(dt) {
         this.ball.pos.x += this.ball.vel.x * dt;
         this.ball.pos.y += this.ball.vel.y * dt;
 
         if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
-            this.ball.vel.x = -this.ball.vel.x;
+            const playerId = this.ball.vel.x < 0 | 0;
+            this.players[playerId].score++;
+            this.reset();
         }
         if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
             this.ball.vel.y = -this.ball.vel.y;
@@ -121,4 +134,8 @@ const pong = new Pong(canvas);
 // Allows player to move with the mouse
 canvas.addEventListener('mousemove', event => {
     pong.players[0].pos.y = event.offsetY;
+});
+
+canvas.addEventListener('click', event => {
+    pong.start();
 });
