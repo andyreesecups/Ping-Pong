1798b0067f6f4bc7f8bad808bbf29183b386aa7b
andyreesecups <andyreesecups@yahoo.com>
2017-08-20 09:02:14 -0500

    added score characters

--- pong.js
+++ pong.js
@@ -5,12 +5,12 @@ class Vec {
         this.y = y;
     }
     get len() {
-    	return Math.sqrt(this.x * this.x + this.y * this.y);
+        return Math.sqrt(this.x * this.x + this.y * this.y);
     }
     set len(value) {
-    	const fact = value / this.len;
-    	this.x *= fact;
-    	this.y *= fact;
+        const fact = value / this.len;
+        this.x *= fact;
+        this.y *= fact;
     }
 }
 
@@ -79,12 +79,45 @@ class Pong {
         };
         callback();
 
+        // Creates score characters
+        this.CHAR_PIXEL = 10;
+        this.CHARS = [
+            '111101101101111',
+            '010010010010010',
+            '111001111100111',
+            '111001111001111',
+            '101101111001001',
+            '111100111001111',
+            '111100111101111',
+            '111001001001001',
+            '111101111101111',
+            '111101111001111',
+        ].map(str => {
+            const canvas = document.createElement('canvas');
+            canvas.height = this.CHAR_PIXEL * 5;
+            canvas.width = this.CHAR_PIXEL * 5;
+            const context = canvas.getContext('2d');
+            context.fillStyle = '#fff';
+            str.split('').forEach((fill, i) => {
+                if (fill === '1') {
+                    context.fillRect(
+                        (i % 3) * this.CHAR_PIXEL,
+                        (i / 3 | 0) * this.CHAR_PIXEL,
+                        this.CHAR_PIXEL,
+                        this.CHAR_PIXEL);
+                }
+            });
+            return canvas;
+        });
+
+
+
         this.reset();
     }
 
     collide(player, ball) {
         if (player.left < ball.right && player.right > ball.left && player.top < ball.bottom && player.bottom > ball.top) {
-        	const len = ball.vel.len;
+            const len = ball.vel.len;
             ball.vel.x = -ball.vel.x;
             ball.vel.y += 300 * (Math.random() - .5);
             ball.vel.len = len * 1.05;
@@ -97,11 +130,29 @@ class Pong {
 
         this.drawRect(this.ball);
         this.players.forEach(player => this.drawRect(player));
+
+        this.drawScore();
     }
 
     drawRect(rect) {
         this._context.fillStyle = "#fff";
         this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
+    }
+
+    drawScore() {
+        const align = this._canvas.width / 3;
+        const CHAR_W = this.CHAR_PIXEL * 4;
+        this.players.forEach((player, index) => {
+            const chars = player.score.toString().split('');
+            const offset = align *
+                (index + 1) -
+                (CHAR_W * chars.length / 2) *
+                this.CHAR_PIXEL / 2;
+            chars.forEach((char, pos) => {
+                this._context.drawImage(this.CHARS[char | 0],
+                    offset + pos * CHAR_W, 20);
+            });
+        });
     }
 
     reset() {
